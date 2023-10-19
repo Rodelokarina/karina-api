1cad1c4b7473de209b2a68f4a96a8d21f33edc7f
Karina Rodelo Cataño <karinarodelo27@gmail.com>
2023-10-19 18:40:33 -0500

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductosModule } from './productos/productos.module';
+import { CustomersModule } from './customers/customers.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductoService } from './producto/producto.service';
+import { ProductosModule } from './productos/productos.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductosModule, AuthModule, PaymentsModule, OrdersModule, InventoryModule, CustomersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProductoService],
 })
 export class AppModule {}
